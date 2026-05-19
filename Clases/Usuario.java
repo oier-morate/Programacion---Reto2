181c9723b3d7c824c37d1460a1daa99e58c0388c
oier-morate <oiertopro@gmail.com>
2026-05-19 13:10:42 +0200

    Añadir el contenido de la clase Usuario

--- Clases/Usuario.java
+++ Clases/Usuario.java
@@ -1,1 +1,39 @@
+public class Usuario {
 
+    private int idUsuario;
+    private String correoElectronico;
+    private String contrasena;
+
+    // Constructor
+    public Usuario(int idUsuario, String correoElectronico, String contrasena) {
+        this.idUsuario = idUsuario;
+        this.correoElectronico = correoElectronico;
+        this.contrasena = contrasena;
+    }
+
+    // Getters
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    // Setters
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
