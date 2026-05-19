535c54e7b740eb0b96e6364447ff57a611afcaa9
oier-morate <oiertopro@gmail.com>
2026-05-19 13:09:01 +0200

    Añadir el contenido en la clase voto

--- Clases/Voto.java
+++ Clases/Voto.java
@@ -1,1 +1,29 @@
+public class Voto {
 
+    private int idVoto;
+    private Usuario idUsuario;
+
+    // Constructor
+    public Voto(int idVoto, Usuario idUsuario) {
+        this.idVoto = idVoto;
+        this.idUsuario = idUsuario;
+    }
+
+    // Getters
+    public int getIdVoto() {
+        return idVoto;
+    }
+
+    public Usuario getIdUsuario() {
+        return idUsuario;
+    }
+
+    // Setters
+    public void setIdVoto(int idVoto) {
+        this.idVoto = idVoto;
+    }
+
+    public void setIdUsuario(Usuario idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+}
